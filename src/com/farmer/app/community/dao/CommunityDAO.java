499625153e0a50601792c8e87fa0e56045c3e612
sudal0216 <sudal0216@gmail.com>
2022-10-18 16:35:46 +0900

    Community 작업 진행(write, list 진행중)

--- src/com/farmer/app/community/dao/CommunityDAO.java
+++ src/com/farmer/app/community/dao/CommunityDAO.java
@@ -3,13 +3,19 @@ package com.farmer.app.community.dao;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import com.farmer.app.community.vo.CommunityVO;
 import com.farmer.mybatis.config.MyBatisConfig;
 
 public class CommunityDAO {
-	 SqlSessionFactory sqlSessionFactory = MyBatisConfig.getSqlsessionFactory();
-	   SqlSession sqlSession;
-	   
-	   public CommunityDAO() {
-		   sqlSession = sqlSessionFactory.openSession(true);
+	SqlSessionFactory sqlSessionFactory = MyBatisConfig.getSqlsessionFactory();
+	SqlSession sqlSession;
+
+	public CommunityDAO() {
+		sqlSession = sqlSessionFactory.openSession(true);
+	}
+	
+	/* 게시글 작성 */
+	public void write(CommunityVO communityVO) {
+		sqlSession.insert("Community.write", communityVO);
 	}
 }
