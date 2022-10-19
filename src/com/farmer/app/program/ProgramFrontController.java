c0068e2efc585bd932e5f4b09c1403ba01c9f62e
yeonily <wluds9791@gmail.com>
2022-10-19 09:15:16 +0900

    김지연 10.19 프로그램 detail 수정

--- src/com/farmer/app/program/ProgramFrontController.java
+++ src/com/farmer/app/program/ProgramFrontController.java
@@ -29,34 +29,28 @@ public class ProgramFrontController extends HttpServlet {
 	    String target = requestURI.substring(contextPath.length());
 	    Result result = null;
 	    
-	      if(target.equals("/program/apply01.pg")) {//프로그램 신청상세페이지 1로 이동
-	    	  result = new Result();
-	    	  result.setPath("/app/program/programDetailPage_1.jsp");
+	      if(target.equals("/program/apply01Ok.pg")) {//프로그램 신청상세페이지 1로 이동
+	    	  result = new apply01OkController().execute(req, resp);
 	          
-	       }
-	      else if(target.equals("/program/applyListOk.pg")) {//프로그램 목록페이지 들어갔을 때, DB에서 프로그램 전체목록 가져오기(SELECT)
+	      }else if(target.equals("/program/applyListOk.pg")) {//프로그램 목록페이지 들어갔을 때, DB에서 프로그램 전체목록 가져오기(SELECT)
 	          result = new ApplyListOkController().execute(req, resp);
-	       }
-	      else if(target.equals("/program/viewCountOk.pg")) {//프로그램 목록페이지 조회순 정렬
+	          
+	      }else if(target.equals("/program/viewCountOk.pg")) {//프로그램 목록페이지 조회순 정렬
 	    	  
-	      }
-	      else if(target.equals("/program/registerOk.pg")) {//프로그램 가장최근등록일순 정렬
+	      }else if(target.equals("/program/registerOk.pg")) {//프로그램 가장최근등록일순 정렬
 		   		
-	       }
-	      else if(target.equals("/program/deadlineOk.pg")) {//프로그램목록 최근 마감일로 정렬
+	      }else if(target.equals("/program/deadlineOk.pg")) {//프로그램목록 최근 마감일로 정렬
 		   		
-	       }
-	      else if(target.equals("/program/apply02.pg")) {//프로그램 신청상세페이지2로 이동
-	   		
-	       }
-	      else if(target.equals("/program/applyCancel.pg")) {//프로그램 신청상세페이지2에서 취소 , 프로그램목록페이지로 이동
+	      }else if(target.equals("/program/apply02.pg")) {//프로그램 신청상세페이지2로 이동
+	    	  result = new Result();
+	    	  result.setPath("/app/program/programDetailPage_2.jsp");
+	    	  		
+	      }else if(target.equals("/program/applyCancel.pg")) {//프로그램 신청상세페이지2에서 취소 , 프로그램목록페이지로 이동
 	    	  
-	      }
-	      else if(target.equals("/program/programWrite.pg")) { //프로그램 등록페이지로 이동
+	      }else if(target.equals("/program/programWrite.pg")) { //프로그램 등록페이지로 이동
 	    	  result = new Result();
 	          result.setPath("/app/program/programRegister.jsp");
-	      }
-	      else if(target.equals("/program/programWriteOk.pg")) { //프로그램 등록하기(INSERT)
+	      }else if(target.equals("/program/programWriteOk.pg")) { //프로그램 등록하기(INSERT)
 	    	  
 	      }
 
